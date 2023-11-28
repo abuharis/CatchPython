aae0e80f58ef10cd141d9b032e9b8fb3f12069e1
Abuharis Salih <abuharis.salih@gmail.com>
2023-11-28 15:10:29 +0200

    Contains magic method

---
+++ Magic methods/contains.py
@@ -0,0 +1,13 @@
+class Items:
+    def __init__(self, items):
+        self.items = items
+
+
+    def __contains__(self, item):
+        print("Searching an item")
+        print(f'"{item}" in {self.items}')
+
+        return item in self.items
+if __name__ == '__main__':
+    my_items = Items([1, 23, 'b'])
+    print('b' in my_items)
